bc3f3e7a738a5a2578d9002494168a01e4e29efd
Askold Astakhov <astahov@element-soft.com>
2023-01-12 20:37:07 +0300

    antd integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import {Button} from 'antd';
 import {useState} from 'react';
 
 import reactLogo from './assets/react.svg';
+
 import './App.css';
 
 function App() {
@@ -18,10 +20,9 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount(count => count + 1)}>
-                    count is
-                    {count}
-                </button>
+                <Button type="primary" onClick={() => setCount(count => count + 1)}>
+                    count is {count}
+                </Button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,22 @@
+import {ConfigProvider} from 'antd';
+import ruRU from 'antd/locale/ru_RU';
+import dayjs from 'dayjs';
 import {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+
 import './index.css';
+
+import 'dayjs/locale/ru';
+import validateMessages from './libs/validateMessages';
+
+dayjs.locale('ru');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
-        <App />
+        <ConfigProvider locale={ruRU} form={{validateMessages}}>
+            <App />
+        </ConfigProvider>
     </StrictMode>,
 );
